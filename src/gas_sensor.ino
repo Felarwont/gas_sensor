8466002f154fb69128c2e8a1019efd3c9325e602
Rushan <karimov29rushan@gmail.com>
2025-12-18 14:55:00 +0500

    Refactor: extract functions from main logic

--- src/gas_sensor.ino
+++ src/gas_sensor.ino
@@ -21,36 +21,66 @@ void setup() {
 
   digitalWrite(ledGreenPin, HIGH);
 
-  lcd.init();
-  lcd.backlight();
+  lcdInitializeAnimation();
 }
 
 void loop() {
   gasValue = analogRead(gasPin);
+  updateLCD();
 
+  if (gasValue > threshold) {
+    alertMode();
+  } else {
+    normalMode();
+  }
+}
+
+void lcdInitializeAnimation() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Gas Sensor");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Initializing");
+
+  for (int i = 0; i < 3; i++) {
+    delay(1000);
+    lcd.print(".");
+  }
+
+  delay(600);
+  lcd.clear();
+}
+
+void updateLCD() {
   lcd.setCursor(0, 0);
   lcd.print("Gas: ");
   lcd.print(gasValue);
   lcd.print("    ");
+}
 
-  if (gasValue > threshold) {
-    lcd.setCursor(0, 1);
-    lcd.print("Status: ALERT ");
+void alertMode() {
+  lcd.setCursor(0, 1);
+  lcd.print("Status: ALERT ");
 
-    for (int f = freqMin; f <= freqMax; f += 3) {
-        tone(buzzerPin, f);
-        digitalWrite(ledRedPin, f > (freqMax + freqMin) / 2);
-        delay(1);
-    }
+  for (int f = freqMin; f <= freqMax; f += 3) {
+    tone(buzzerPin, f);
+    digitalWrite(ledRedPin, f > (freqMax + freqMin) / 2);
+    delay(1);
+  }
 
-    for (int f = freqMax; f >= freqMin; f -= 3) {
-        tone(buzzerPin, f);
-        digitalWrite(ledRedPin, f > (freqMax + freqMin) / 2);
-        delay(1);
-    }
-  } else {
-    lcd.setCursor(0, 1);
-    lcd.print("Status: OK    ");
-    noTone(buzzerPin);
-    digitalWrite(ledRedPin, LOW);
+  for (int f = freqMax; f >= freqMin; f -= 3) {
+    tone(buzzerPin, f);
+    digitalWrite(ledRedPin, f > (freqMax + freqMin) / 2);
+    delay(1);
+  }
+}
+
+void normalMode() {
+  lcd.setCursor(0, 1);
+  lcd.print("Status: OK    ");
+  noTone(buzzerPin);
+  digitalWrite(ledRedPin, LOW);
 }
