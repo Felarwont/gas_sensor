a160e8e847fba46937ba976ff5d153a4585891eb
Rushan <karimov29rushan@gmail.com>
2025-12-18 14:03:00 +0500

    Added display on the LCD

--- src/gas_sensor.ino
+++ src/gas_sensor.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 const int gasPin = A1;
 const int ledRedPin = 2;
 const int ledGreenPin = 3;
@@ -9,22 +12,35 @@ const int freqMax = 3850;
 
 uint16_t gasValue;
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   pinMode(ledRedPin, OUTPUT);
   pinMode(ledGreenPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
   digitalWrite(ledGreenPin, HIGH);
+
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
   gasValue = analogRead(gasPin);
 
+  lcd.setCursor(0, 0);
+  lcd.print("Gas: ");
+  lcd.print(gasValue);
+  lcd.print("    ");
+
   if (gasValue > threshold) {
+    lcd.setCursor(0, 1);
+    lcd.print("Status: ALERT ");
+
     for (int f = freqMin; f <= freqMax; f += 3) {
-    tone(buzzerPin, f);
-    digitalWrite(ledRedPin, f > (freqMax + freqMin) / 2);
-    delay(1);
+        tone(buzzerPin, f);
+        digitalWrite(ledRedPin, f > (freqMax + freqMin) / 2);
+        delay(1);
     }
 
     for (int f = freqMax; f >= freqMin; f -= 3) {
@@ -33,6 +49,8 @@ void loop() {
         delay(1);
     }
   } else {
+    lcd.setCursor(0, 1);
+    lcd.print("Status: OK    ");
     noTone(buzzerPin);
     digitalWrite(ledRedPin, LOW);
 }
